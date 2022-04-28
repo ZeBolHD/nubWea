8ba19002944aed74b81ee41a234da784924c678a
ZeBolHD <zezebolz@gmail.com>
2022-04-28 10:11:05 +0300

    dodelani datchiki

--- src/IUS/Datchik.java
+++ src/IUS/Datchik.java
@@ -11,22 +11,36 @@ public class Datchik {
     static Random random = new Random();
 
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        Random random = new Random();
+        //Заводим датчики в постостоянный цикл
         while(true) {
             datchik1();datchik2(); datchik3();datchik4(); datchik5();
         }
     }
+
+    //Создаём датчики и присваеваем им собственные значения
+
      public static void datchik1(){
+
         int min = 22;
         int max = 55;
         int a = 5;
-        int r = random.nextInt(max - a - (min-a) + min);
+
+         //Получаем случайное число в диапозоне: min-a<x<max+a
+
+        int r = random.nextInt(max + a - (min-a)) + min-a;
         try {
+
+            //Программа застывает на 3000мс
+
             Thread.sleep(3000);
-            if(r<min || r>max) {
-                System.out.println(r);
-            }else {
+
+            //Сверяем число с введённой температурой
+
+            if(r<min) {
+                System.out.println("Темпетура ниже нормы. Температура: " + r);
+            }else if(r>max){
+                System.out.println("Темпетаруа выше нормы. Температура: " + r);
+            } else {
                 System.out.println("Всё в норме. Температура: " + r);
             }
         } catch (InterruptedException e) {
@@ -35,15 +49,19 @@ public class Datchik {
      }
 
     public static void datchik2(){
+
         int min = 36;
         int max = 127;
-        int a = 5;
-        int r = random.nextInt(max - a - (min-a) + min);
+        int a = 10;
+        int r = random.nextInt(max + a - (min-a)) + min-a;
+
         try {
             Thread.sleep(4000);
-            if(r<min || r>max) {
-                System.out.println(r);
-            }else {
+            if(r<min) {
+                System.out.println("Темпетура ниже нормы. Температура: " + r);
+            }else if(r>max){
+                System.out.println("Темпетаруа выше нормы. Температура: " + r);
+            } else {
                 System.out.println("Всё в норме. Температура: " + r);
             }
         } catch (InterruptedException e) {
@@ -51,15 +69,19 @@ public class Datchik {
         }
     }
     public static void datchik3(){
+
         int min = 38;
         int max = 55;
-        int a = 5;
-        int r = random.nextInt(max - a - (min-a) + min);
+        int a = 7;
+        int r = random.nextInt(max + a - (min-a)) + min-a;
+
         try {
-            Thread.sleep(3000);
-            if(r<min || r>max) {
-                System.out.println(r);
-            }else {
+            Thread.sleep(1000);
+            if(r<min) {
+                System.out.println("Темпетура ниже нормы. Температура: " + r);
+            }else if(r>max){
+                System.out.println("Темпетаруа выше нормы. Температура: " + r);
+            } else {
                 System.out.println("Всё в норме. Температура: " + r);
             }
         } catch (InterruptedException e) {
@@ -67,15 +89,19 @@ public class Datchik {
         }
     }
     public static void datchik4(){
+
         int min = 10;
         int max = 34;
-        int a = 5;
-        int r = random.nextInt(max - a - (min-a) + min);
+        int a = 8;
+        int r = random.nextInt(max + a - (min-a)) + min-a;
+
         try {
-            Thread.sleep(3000);
-            if(r<min || r>max) {
-                System.out.println(r);
-            }else {
+            Thread.sleep(1500);
+            if(r<min) {
+                System.out.println("Темпетура ниже нормы. Температура: " + r);
+            }else if(r>max){
+                System.out.println("Темпетаруа выше нормы. Температура: " + r);
+            } else {
                 System.out.println("Всё в норме. Температура: " + r);
             }
         } catch (InterruptedException e) {
@@ -83,15 +109,19 @@ public class Datchik {
         }
     }
     public static void datchik5(){
+
         int min = 54;
         int max = 129;
-        int a = 5;
-        int r = random.nextInt(max - a - (min-a) + min);
+        int a = 23;
+        int r = random.nextInt(max + a - (min-a)) + min-a;
+
         try {
-            Thread.sleep(3000);
-            if(r<min || r>max) {
-                System.out.println(r);
-            }else {
+            Thread.sleep(6000);
+            if(r<min) {
+                System.out.println("Темпетура ниже нормы. Температура: " + r);
+            }else if(r>max){
+                System.out.println("Темпетаруа выше нормы. Температура: " + r);
+            } else {
                 System.out.println("Всё в норме. Температура: " + r);
             }
         } catch (InterruptedException e) {
