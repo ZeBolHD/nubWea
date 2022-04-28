4bd41d5b0ddeb4bb933c6ef07b653fc647457f0f
ZeBolHD <zezebolz@gmail.com>
2022-04-28 11:42:34 +0300

    datchiki final

--- src/IUS/Datchik.java
+++ src/IUS/Datchik.java
@@ -16,44 +16,43 @@ public class Datchik {
         }
     }
 
-    //Создаём метод, чтобы сверить температуру
+    //Создаём метод, в котором получаем случайное значение r в диапазоне max+a<x<min-a, производится задержка по
+    //заданному периоду опроса и проверка температуры
 
-    public static void temp(int min, int max, int r, int time){
-        time /= 1000;
-        if(r<min) {
-            System.out.println("Темпетура ниже нормы. Температура: " + r + "°C. Период опроса: " + time + "сек.");
-        }else if(r>max){
-            System.out.println("Темпетаруа выше нормы. Температура: " + r + "°C. Период опроса: " + time + "сек.");
-        } else {
-            System.out.println("Всё в норме. Температура: " + r + "°C. Период опроса: " + time + "сек.");
+
+    public static void temp(int min, int max, int a, int time){
+
+        int r = random.nextInt(max + a - (min-a)) + min-a;
+
+        try {
+            Thread.sleep(time);
+            time /= 1000;
+            if (r < min) {
+                System.out.println("Температура ниже нормы. Температура: " + r + "°C. Период опроса: " + time + " сек.");
+            } else if (r > max) {
+                System.out.println("Температура выше нормы. Температура: " + r + "°C. Период опроса: " + time + " сек.");
+            } else {
+                System.out.println("Всё в норме. Температура: " + r + "°C. Период опроса: " + time + " сек.");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     //Создаём датчики и присваеваем им собственные значения
 
      public static void datchik1(){
-
         int min = 22;
         int max = 55;
         int a = 5;
 
-         //Задаём период опроса в мс
+        //Задаём период опроса в мc
 
          int time = 3000;
 
-         //Получаем случайное число в диапозоне: min-a<x<max+a
+         //Обращаемся к методу temp
 
-        int r = random.nextInt(max + a - (min-a)) + min-a;
-        try {
-
-            Thread.sleep(time);
-
-            //Обращаемся к методу temp
-
-            temp(min, max, r, time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+         temp(min, max, a, time);
      }
 
     public static void datchik2(){
@@ -61,17 +60,10 @@ public class Datchik {
         int min = 36;
         int max = 127;
         int a = 10;
-        int r = random.nextInt(max + a - (min-a)) + min-a;
 
         int time = 2000;
 
-        try {
-            Thread.sleep(time);
-
-            temp(min, max, r, time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        temp(min, max, a, time);
     }
 
     public static void datchik3(){
@@ -79,17 +71,10 @@ public class Datchik {
         int min = 38;
         int max = 55;
         int a = 7;
-        int r = random.nextInt(max + a - (min-a)) + min-a;
 
         int time = 1000;
 
-        try {
-            Thread.sleep(time);
-
-            temp(min, max, r, time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        temp(min, max, a, time);
     }
 
     public static void datchik4(){
@@ -97,33 +82,20 @@ public class Datchik {
         int min = 10;
         int max = 34;
         int a = 8;
-        int r = random.nextInt(max + a - (min-a)) + min-a;
 
         int time = 4000;
 
-        try {
-            Thread.sleep(time);
-
-            temp(min, max, r, time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        temp(min, max, a, time);
     }
     public static void datchik5(){
 
         int min = 54;
         int max = 129;
         int a = 23;
-        int r = random.nextInt(max + a - (min-a)) + min-a;
 
         int time = 5000;
 
-        try {
-            Thread.sleep(time);
-            temp(min, max, r, time);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        temp(min, max, a, time);
     }
 
 }
